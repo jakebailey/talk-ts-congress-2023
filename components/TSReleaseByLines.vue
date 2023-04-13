ed89e5f7bb5157c143736202d52903ea4e81fc7d
Jake Bailey <5341706+jakebailey@users.noreply.github.com>
2023-04-12 20:30:31 -0700

    Charting

---
+++ components/TSReleaseByLines.vue
@@ -0,0 +1,107 @@
+<template>
+  <Line :data="data" :options="options" />
+</template>
+
+<script lang="ts">
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+} from "chart.js";
+import { Line } from "vue-chartjs";
+
+ChartJS.register(CategoryScale, LinearScale, PointElement, LineElement, Title, Tooltip, Legend);
+
+export default {
+  name: "TSReleaseByLines",
+  components: {
+    Line,
+  },
+  props: ["theme"],
+  data() {
+    const data: [label: string, value: number][] = [
+      ["0.8", 31691],
+      ["0.9", 70493],
+      ["1.0", 70470],
+      ["1.1", 71599],
+      ["1.3", 74157],
+      ["1.4", 77156],
+      ["1.5", 64389],
+      ["1.6", 70617],
+      ["1.7", 70827],
+      ["1.8", 75482],
+      ["2.0", 101437],
+      ["2.1", 116300],
+      ["2.2", 121649],
+      ["2.3", 127394],
+      ["2.4", 130602],
+      ["2.5", 134393],
+      ["2.6", 147204],
+      ["2.7", 153366],
+      ["2.8", 156955],
+      ["2.9", 167072],
+      ["3.0", 173682],
+      ["3.1", 180730],
+      ["3.2", 187227],
+      ["3.3", 195005],
+      ["3.4", 195438],
+      ["3.5", 197774],
+      ["3.6", 206488],
+      ["3.7", 215547],
+      ["3.8", 224261],
+      ["3.9", 229617],
+      ["4.0", 240172],
+      ["4.1", 244368],
+      ["4.2", 249624],
+      ["4.3", 261415],
+      ["4.4", 265228],
+      ["4.5", 269168],
+      ["4.6", 271921],
+      ["4.7", 275858],
+      ["4.8", 279077],
+      ["4.9", 280780],
+      ["5.0", 306081],
+    ];
+
+    return {
+      data: {
+        labels: data.map((x) => x[0]),
+        datasets: [
+          {
+            label: "Lines of code",
+            backgroundColor: this.theme === "dark" ? "#ff5722" : "#285577",
+            borderColor: this.theme === "dark" ? "#451100" : "#91bada",
+            data: data.map((x) => x[1]),
+          },
+        ],
+      },
+      options: {
+        responsive: true,
+        devicePixelRatio: 4,
+        animation: {
+          duration: 0,
+        },
+        scales: {
+          x: {
+            grid: {
+              color: this.theme === "dark" ? "#222" : undefined,
+            },
+          },
+          y: {
+            beginAtZero: true,
+            grid: {
+              color: this.theme === "dark" ? "#222" : undefined,
+            },
+          },
+        },
+        // maintainAspectRatio: false,
+      },
+    };
+  },
+};
+</script>
--- slides.md
+++ slides.md
@@ -10,17 +10,40 @@ layout: cover
 
 ---
 
+# Outline
+
+-
+
+---
+
 # A little history...
 
-```ts
-namespace ts {
-    export function createSourceFile(...) {
-        // ...
-    }
-}
-```
+This slide sucks.
 
-foo:
+- 2012
+  - TypeScript announced! (v0.8)
+  - DefinitelyTyped! (Yes, this early!)
+- ...
+- 2014
+  - v1.1 - TypeScript _rewritten_
+  - v1.3 - Tuple types
+  - v1.4 - Unions (after tuples???), `let`/`const`, template strings
+  - v1.5 - ES module syntax (`import`/`export`), decorators
+- ...
+- 2016
+  - v2.0 - control-flow analysis (CFA), `undefined`, `null`
+- ...
+- 2022
+  - v5.0 dev begins
+  - Migration to modules!
 
-- bar
-  - baz
+---
+
+# TS is big!
+
+<!-- TODO: use LightOrDark -->
+
+<LightOrDark>
+<template #dark><TSReleaseByLines theme="dark" /></template>
+<template #light><TSReleaseByLines theme="light" /></template>
+</LightOrDark>
